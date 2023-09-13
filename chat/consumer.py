e6cff83a99aed66f31f547a30e0126abd8b78e05
rashid kp <rashid.kp484@gmail.com>
2023-09-14 02:19:28 +0530

    chat api sapmle develepod

---
+++ chat/consumer.py
@@ -0,0 +1,20 @@
+import json
+
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+
+    def connect(self):
+        print("connecting ....")
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        print(message)
+        print("returning message back to the user")
+        self.send(text_data=json.dumps({"message": message}))
+        
--- config/asgi.py
+++ config/asgi.py
@@ -9,14 +9,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import chat.routing
+
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     }
 )
